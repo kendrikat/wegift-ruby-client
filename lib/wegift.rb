f7a0e3a59877456284f3e68abe1f2dd8f7484709
Klaas Endrikat <klaas.endrikat@googlemail.com>
2017-10-17 15:27:11 +0200

    require fix

--- lib/wegift.rb
+++ lib/wegift.rb
@@ -1,4 +1,11 @@
+require 'wegift/version'
 require 'wegift/client'
+
+require 'wegift/models/initializable'
+require 'wegift/models/response'
+require 'wegift/models/product'
+require 'wegift/models/products'
+require 'wegift/models/order'
 
 module Wegift
 
