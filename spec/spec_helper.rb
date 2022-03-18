18e246278fd89f4eee8c6d1cdf5d895beb8869b9
Chi Leung <chi.leung@mo.work>
2022-03-18 21:53:58 +0000

    Exclude sensitive data from tapes

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -25,6 +25,11 @@ end
 VCR.configure do |c|
   c.cassette_library_dir = 'spec/tapes'
   c.hook_into :webmock
+
+  c.before_record do |i|
+    i.response.headers.delete("Set-Cookie")
+    i.request.headers.delete("Authorization")
+  end
 end
 
 RSpec.configure do |config|
