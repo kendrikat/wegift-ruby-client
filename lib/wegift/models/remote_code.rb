94b28d4a4656000f653cfda28e322ddf4e2cd2ce
Chi Leung <chi.leung@mo.work>
2022-04-19 11:34:21 +0100

    Use class initializable

--- lib/wegift/models/remote_code.rb
+++ lib/wegift/models/remote_code.rb
@@ -5,15 +5,11 @@ require 'uri'
 
 module Wegift
   class RemoteCode < Response
-    attr_reader :url
+    attr_accessor :url
 
     # response/success
-    attr_reader :amount, :barcode_format, :barcode_string, :code, :expiry_date,
+    attr_accessor :amount, :barcode_format, :barcode_string, :code, :expiry_date,
       :pin, :type
-
-    def initialize(url:)
-      @url = url
-    end
 
     def self.get(url)
       uri = URI(url)
@@ -24,7 +20,7 @@ module Wegift
       end
 
       remote_code = self.new(url: url)
-      remote_code.parse(response)
+      remote_code = remote_code.parse(response)
 
       remote_code
     end
@@ -33,13 +29,9 @@ module Wegift
       super
 
       if is_successful?
-        @amount = @payload['e_code']['amount']
-        @barcode_format = @payload['e_code']['barcode_format']
-        @barcode_string = @payload['e_code']['barcode_string']
-        @code = @payload['e_code']['code']
-        @expiry_date = @payload['e_code']['expiry_date']
-        @pin = @payload['e_code']['pin']
-        @type = @payload['e_code']['type']
+        self.class.new(@payload['e_code'])
+      else
+        self
       end
     end
   end
